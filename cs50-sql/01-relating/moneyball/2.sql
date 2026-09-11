4899cabb136bcd12f7454c3a2c2388f60f784c23
Gustavo Orellana <juangustavoore@gmail.com>
2026-09-11 16:38:24 -0500

    Add Week 1 Moneyball Solution

---
+++ cs50-sql/01-relating/moneyball/2.sql
@@ -0,0 +1,7 @@
+SELECT "salaries"."year", "salaries"."salary"
+FROM "salaries"
+JOIN "players"
+    ON "players"."id" = "salaries"."player_id"
+WHERE "players"."first_name" = 'Cal'
+    AND "players"."last_name" = 'Ripken'
+ORDER BY "salaries"."year" DESC;
