4c228573345e7ec66f6a30f0f0015cb361b3c0d0
Gustavo Orellana <juangustavoore@gmail.com>
2026-09-11 16:04:07 -0500

    Add Week 1 DESE Solution

---
+++ cs50-sql/01-relating/dese/12.sql
@@ -0,0 +1,16 @@
+SELECT "districts"."name","expenditures"."per_pupil_expenditure", "staff_evaluations"."exemplary"
+FROM "districts"
+JOIN "expenditures"
+    ON "expenditures"."district_id" = "districts"."id"
+JOIN "staff_evaluations"
+    ON "staff_evaluations"."district_id" = "districts"."id"
+WHERE "expenditures"."per_pupil_expenditure" > (
+    SELECT AVG("per_pupil_expenditure")
+    FROM "expenditures"
+)
+    AND "staff_evaluations"."exemplary" > (
+        SELECT AVG("exemplary")
+        FROM "staff_evaluations"
+    )
+    AND "districts"."type" = 'Public School District'
+ORDER BY "staff_evaluations"."exemplary" DESC,"expenditures"."per_pupil_expenditure" DESC;
