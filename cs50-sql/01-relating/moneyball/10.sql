036b88d838e7a9be082feda177e63e75048b1281
Gustavo Orellana <juangustavoore@gmail.com>
2026-09-13 16:51:31 -0500

    Add Week 1 Moneyball Solution

---
+++ cs50-sql/01-relating/moneyball/10.sql
@@ -0,0 +1,13 @@
+SELECT
+    "players"."first_name",
+    "players"."last_name",
+    "salaries"."salary",
+    "performances"."HR",
+    "performances"."year"
+FROM "players"
+JOIN "salaries"
+    ON "salaries"."player_id" = "players"."id"
+JOIN "performances"
+    ON "performances"."player_id" = "players"."id"
+    AND "performances"."year" = "salaries"."year"
+ORDER BY "players"."id", "performances"."year" DESC, "performances"."HR" DESC, "salaries"."salary" DESC;
