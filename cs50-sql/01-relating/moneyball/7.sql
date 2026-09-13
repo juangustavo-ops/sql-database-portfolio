a147b6bfafb6d41372b6db46925815a0b0bc5dd6
Gustavo Orellana <juangustavoore@gmail.com>
2026-09-13 16:46:08 -0500

    Add Week 1 Moneyball Solution

---
+++ cs50-sql/01-relating/moneyball/7.sql
@@ -0,0 +1,8 @@
+SELECT "players"."first_name", "players"."last_name"
+FROM "players"
+JOIN "salaries"
+    ON "salaries"."player_id" = "players"."id"
+WHERE "salaries"."salary" = (
+    SELECT MAX("salaries"."salary")
+    FROM "salaries"
+);
