c3e1180b68e1368b38bc29c282203dfe2f75b438
Gustavo Orellana <juangustavoore@gmail.com>
2026-09-14 12:01:36 -0500

    Add Week 4 Bed and Breakfast Solution

---
+++ cs50-sql/04-viewing/bnb/june_vacancies.sql
@@ -0,0 +1,8 @@
+CREATE VIEW "june_vacancies" AS
+SELECT "listings"."id", "property_type", "host_name", COUNT("availabilities"."available") AS "days_vacant"
+FROM "listings"
+JOIN "availabilities"
+    ON "listings"."id" = "availabilities"."listing_id"
+    AND "date" LIKE '2023-06-%'
+    AND "available" = 'TRUE'
+GROUP BY "listings"."id";
